df3f6507e6487204af5f7f111c9e152b3c1fc502
lakhanji mishra <76896079+lakhanji@users.noreply.github.com>
2024-12-09 10:28:31 +0530

    Update Update.jsx

--- frontend/src/components/todo/Update.jsx
+++ frontend/src/components/todo/Update.jsx
@@ -19,7 +19,7 @@ const Update = ({ display, update }) => {
   };
   const submit = async () => {
     await axios
-      .put(`${window.location.origin}http:///api/v2/updateTask/${update._id}`, Inputs)
+      .put(`${window.location.origin}/api/v2/updateTask/${update._id}`, Inputs)
       .then((response) => {
         toast.success(response.data.message);
       });
